68ece8b8b858fdb1bd8725b5f45fa3573037123a
Роман Чурсанов <MorysovPlay@mail.ru>
2019-04-09 18:05:48 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace New_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label = new System.Windows.Forms.Label();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F);
+            this.label.Location = new System.Drawing.Point(143, 13);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(153, 34);
+            this.label.TabIndex = 0;
+            this.label.Text = "Укажите путь";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TextBox
+            // 
+            this.TextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TextBox.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBox.Location = new System.Drawing.Point(11, 50);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(416, 26);
+            this.TextBox.TabIndex = 1;
+            this.TextBox.TabStop = false;
+            this.TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(111, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Информация о файле:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(416, 106);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 20;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.Gainsboro;
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.Black;
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.DodgerBlue;
+            this.bunifuThinButton21.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "Выбрать файл";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.SteelBlue;
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 20;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.SystemColors.ControlText;
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.Gainsboro;
+            this.bunifuThinButton21.Location = new System.Drawing.Point(143, 84);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(153, 37);
+            this.bunifuThinButton21.TabIndex = 5;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.BunifuThinButton21_Click);
+            // 
+            // button
+            // 
+            this.button.Enabled = false;
+            this.button.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F);
+            this.button.Location = new System.Drawing.Point(151, 257);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(145, 31);
+            this.button.TabIndex = 6;
+            this.button.TabStop = false;
+            this.button.Text = "Удалить";
+            this.button.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(438, 299);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.bunifuThinButton21);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Terminator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace New_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Метод для выдачи готового оповещения 
+        void Mess(string line1, string line2)
+        {
+            MessageBox.Show(line1, line2, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Метод удаления файла
+        void FileDel(string line)
+        {
+            // При отсутствии файла выводим сообщение об ошибке и очищаем поля от информации
+            if (!File.Exists(line))
+            {
+                MessageBox.Show("Файл не найден", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                TextBox.Text = null;
+                label2.Visible = false;
+                label2.Text = null;
+            }
+            else
+            {
+                // При наличии файла спрашиваем у пользователя о его решении
+                DialogResult result = MessageBox.Show("Вы уверены?", null, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    File.Delete(line);
+                    if (!File.Exists(line))
+                    {
+                        // Если пользователь ответил согласием - удаляем файл и выводим оповещение об удалении
+                        Mess("Файл успешно удален", "");
+                        TextBox.Text = null;
+                        label1.Visible = false;
+                        label2.Text = null;
+                        button.Enabled = false;
+                    }
+                    else
+                    {
+                        // Если произошла ошибка и файл по какой-то причине не удалось удалить - выводим оповещение об ошибке
+                        Mess("Произошел сбой при попытке удалить файл", "Ошибка!");
+                    }
+                }
+                else
+                {
+                    //Если пользователь отказался удалять файл
+                    Mess("Отмена операции", "Внимание!");
+                }
+            }
+        }
+
+        // Событие загрузки формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip t = new ToolTip();
+            t.SetToolTip(TextBox, "Введите полный путь");
+            t.SetToolTip(bunifuThinButton21, "Открыть меню выбор файла");
+            label1.Visible = false;
+        }
+
+        // Диалоговое окно с выбором файла и запись пути в textbox
+        private void BunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myFile = new OpenFileDialog
+            {
+                Title = "Выбор файла"
+            };
+            if (myFile.ShowDialog() == DialogResult.OK)
+            {
+                TextBox.Text = Convert.ToString(myFile.FileName);
+            }
+        }
+
+        //Информация о файле при вставке пути на файл вручную
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Проверка на наличие чего-либо в textbox
+            if (TextBox.Text != null)
+            {
+                TextBox.Text = TextBox.Text.Trim('"');
+                button.Enabled = true;
+                // Проверка файла на существование и выдача информации
+                if(File.Exists(TextBox.Text))
+                {
+                    label1.Visible = true;
+                    label2.Text = $"Создан: " + File.GetCreationTime(TextBox.Text) +
+                                   "\n Открывался: " + File.GetLastAccessTime(TextBox.Text) +
+                                   "\n Изменялся: " + File.GetLastWriteTime(TextBox.Text) +
+                                   "\n Размер файла: " + (Math.Round(Convert.ToDouble(File.ReadAllBytes(TextBox.Text).Length) / 1048576.0, 2)) + " мбайт";
+                }
+                else
+                {
+                    label1.Visible = false;
+                    label2.Text = null;
+                }
+            }
+            //Если textbox стал пустым - скрываем лишнюю информацию
+            else
+            {
+                label1.Visible = false;
+                label2.Text = null;
+                button.Enabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            FileDel(TextBox.Text);
+        }
+    }
+}
